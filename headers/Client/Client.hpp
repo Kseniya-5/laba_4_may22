1f1cc195a47f827e23b220af3774820fc6bd87fe
Kseniya <ksbalitskaya@edu.hse.ru>
2022-06-16 02:38:50 +0300

    add noexcept in Client

--- headers/Client/Client.hpp
+++ headers/Client/Client.hpp
@@ -1,43 +1,47 @@
 #pragma once
+
 #include "Order.hpp"
 
 #ifndef LABA_4_MAY22_CLIENT_HPP
 #define LABA_4_MAY22_CLIENT_HPP
 
-class Client{
+class Client {
 public:
-    void earn_money(double money){
+    void earn_money(double money) noexcept {
         m_balance.add_money(money);
     }
 
-    bool pay_and_receive_order(Order& order){
-        if(!m_balance.waste_money(order.get_cost())){
-            std::cout <<"Not enough money to pay for order!" << std::endl;
+    bool pay_and_receive_order(Order &order) {
+        if (!m_balance.waste_money(order.get_cost())) {
+            std::cout << "Not enough money to pay for order!" << std::endl;
             return false;
         }
         std::cout << "The order is received!" << std::endl;
         order.clear();
         return true;
     }
+
 private:
-    class Balance{
+    class Balance {
     public:
-        bool waste_money(double money){
-            if(m_money >= money){
+        bool waste_money(double money) noexcept {
+            if (m_money >= money) {
                 m_money -= money;
                 return true;
             }
             return false;
         }
 
-        void add_money(double money){
+        void add_money(double money) {
             m_money += money;
         }
 
     private:
         double m_money = 0;
 
-};
+    };
+
     Balance m_balance;
 };
+
 #endif //LABA_4_MAY22_CLIENT_HPP
